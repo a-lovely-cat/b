b765bd7102ddfd0640e307fd32c362994d8e5a32
a-lovely-cat <orz5000z@gmail.com>
2021-01-31 10:23:28 +0200

    added some stuff

--- tennis_client.py
+++ tennis_client.py
@@ -23,14 +23,17 @@ class TennisServer:
         this function recieves pings and sends pongs to all clients available
         """
         for sock in self.clients_poll.poll():
-            sock = socket.socket(fileno=sock[0])
-            client_answer = sock.recv(consts.MAX_RECEIVE_AMOUNT)
-            if client_answer != consts.PING_MESSAGE_VALUE:
+            client_answer = None
+            if sock[1] == select.POLLIN:
+                client_sock = socket.socket(fileno=sock[0])
+                client_answer = client_sock.recv(consts.MAX_RECEIVE_AMOUNT)
+            if client_answer != consts.PING_MESSAGE_VALUE and client_answer is not None:
                 logging.error(f'{self.client_sockets[sock]} has been disconnected')
                 del self.client_sockets[sock]
-                sock.close()
+                self.clients_poll.unregister(sock[0])
+                client_sock.close()
             else:
-                sock.send(consts.PONG_MESSAGE_VALUE)
+                client_sock.send(consts.PONG_MESSAGE_VALUE)
 
     def accept_client(self):
         """
@@ -43,6 +46,7 @@ class TennisServer:
 
         time.sleep(0.1)
         while select.select([self.server_socket], [], [], 0)[0] != []:
+            print(1)
             client_socket, client_address = self.server_socket.accept()
             self.client_sockets[client_socket] = client_address
             self.clients_poll.register(client_socket)
